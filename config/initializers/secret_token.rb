244bf98bf82e3cfab3c725dd0bdf2a77f81d8114
yuichi tanaka <u1tnk1123@gmail.com>
2012-04-28 21:55:44 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jogcalc::Application.config.secret_token = '5d1cab63be3dd0757d190174f3f8e077758fb0cd949414f06bec340b1903ad20b1cea35afcc1a57fb042d4b482140c3219d7dce121ee2363313632d40d5d3fbf'
