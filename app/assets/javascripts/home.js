df76bc242401297c0969bcc18a70db95a8f197fb
yuichi tanaka <u1tnk1123@gmail.com>
2012-04-29 14:42:05 +0900

    入力ロジックがだいたい

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,45 +1,90 @@
+var distance = 0;
+var hours = 0;
+var minutes = 0;
+
+var distancePanelShow;
+var distanceView;
+var distancePanel;
+var distanceInput;
+var timePanelShow;
+var hoursView;
+var hoursInput;
+var timeDecide;
+var minutesView;
+var minutesInput;
+
+var execCalc;
+var speedView;
+
+function clearExeccPanel(){
+    execPanel.hide();
+    speedView.text("");
+}
+function updateDistance(value){
+    distance = value;
+    distanceView.text(distance);
+    distancePanel.hide();
+    checkShowExecutePanel();
+}
+function checkShowExecutePanel(){
+    if(distance === 0 || (hours === 0 && minutes === 0)){
+        clearExeccPanel();
+        return;
+    }
+    execPanel.show();
+}
 $(function(){
-    var distance = 0;
-    var hours = 0;
-    var minutes = 0;
-
-    var distanceView = $('#distanceView');
-    var distanceInput = $('#distanceInput');
-    var hoursView = $("#hoursView");
-    var hoursInput = $("#hoursInput");
-    var minutesView = $("#minutesView");
-    var minutesInput = $("#minutesInput");
+    distanceView = $('#distanceView');
+    distancePanel = $('#distancePanel');
+    distancePanelShow = $('#distancePanelShow');
+    distanceInput = $('#distanceInput');
+    timePanel = $('#timePanel');
+    timePanelShow = $('#timePanelShow');
+    timeDecide = $("#timeDecide");
+    hoursView = $(".hoursView");
+    hoursInput = $("#hoursInput");
+    minutesView = $(".minutesView");
+    minutesInput = $("#minutesInput");
+    execPanel = $('#execPanel');
     
-    var execCalc = $("#execCalc");
-    var speedView = $("#speedView");
+    execCalc = $("#execCalc");
+    speedView = $("#speedView");
 
+    distancePanelShow.click(function(){
+        timePanel.hide();
+        clearExeccPanel();
+        distancePanel.show();
+    });
+    timePanelShow.click(function(){
+        distancePanel.hide();
+        clearExeccPanel();
+        timePanel.show();
+    });
     distanceInput.change(function(){
         if(!distanceInput.val()){
             return
         }
-        distance = parseInt(distanceInput.val());
-        distanceView.text(distance);
+        updateDistance(distanceInput.val());
     });
     hoursInput.change(function(){
         hours = parseInt(hoursInput.val());
-        console.log(hours);
         hoursView.text(hours);
     });
     minutesInput.change(function(){
         minutes = parseInt(minutesInput.val());
         minutesView.text(minutes);
     });
+    timeDecide.click(function(){
+        timePanel.hide();
+        checkShowExecutePanel();
+    });
 
     execCalc.click(function(){
-        if(distance === 0 || (hours === 0 && minutes === 0)){
-            alert("pleas input!");
-            return;
-        }
         var allSecond = hours * 60 * 60 + minutes * 60;
         var resultAllSecond = allSecond / distance;
         var resultMinutes = parseInt(resultAllSecond / 60);
         var resultSecond = parseInt(resultAllSecond % 60);
-        speedView.text("1kmあたり" + resultMinutes + "分" +  resultSecond + "秒で走れ！");
+        speedView.text("1kmあたり" + resultMinutes + "分" +  resultSecond + "秒");
     });
 
 
