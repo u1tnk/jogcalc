787d643bf732152e020f4b79e60650a4b83efe6c
yuichi tanaka <u1tnk1123@gmail.com>
2012-04-29 09:39:33 +0900

    ログイン可能なところまで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
   def callback
-    raise request.env["omniauth.auth"].to_yaml
+    #omniauth.auth環境変数を取得
+    auth = request.env["omniauth.auth"]
+
+    #Userモデルを検索
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) 
+
+    unless user 
+      user = User.create_with_omniauth(auth)
+    end
+
+     session[:user_id] = user.id
+     redirect_to root_url, :notice => "Log in"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Log out"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120428143716) do
+ActiveRecord::Schema.define(:version => 20120428154407) do
 
-  create_table "omniusers", :force => true do |t|
+  create_table "users", :force => true do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.string   "screen_name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
