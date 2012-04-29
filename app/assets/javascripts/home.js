2cc0101d33658aa3944ae51717d2a704ed4f121f
yuichi tanaka <u1tnk1123@gmail.com>
2012-04-29 11:30:04 +0900

    自動更新表示部分

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,33 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(function(){
+    var distance, hours, minutes;
+    var distanceView = $('#distanceView');
+    var distanceInput = $('#distanceInput');
+    var hoursView = $("#hoursView");
+    var hoursInput = $("#hoursInput");
+    var minutesView = $("#minutesView");
+    var minutesInput = $("#minutesInput");
+    
+    var speedView = $("#speedView");
+
+    distanceInput.change(function(){
+        console.log(distanceInput.val())
+        if(!distanceInput.val()){
+            return
+        }
+        distance = distanceInput.val();
+        distanceView.text(distance);
+    });
+    hoursInput.change(function(){
+        hours = hoursInput.val();
+        console.log(hours);
+        hoursView.text(hours);
+    });
+    minutesInput.change(function(){
+        minutes = minutesInput.val();
+        minutesView.text(minutes);
+    });
+
+
+
+});
+
